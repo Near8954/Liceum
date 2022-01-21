808cf7d2b1309f4fcbb2d3499c94881d936f6ed2
Near8954 <97964369+Near8954@users.noreply.github.com>
2022-01-21 19:31:45 +0500

    Счёт за обед

--- функции.py/Счёт за обед.py
+++ функции.py/Счёт за обед.py
@@ -1,1 +1,11 @@
 
+
+def count_food(days):
+    summ = 0
+    for i in days:
+        summ += daily_food[i - 1]
+    return summ
+
+
+daily_food = [0, 150, 150]
+print(count_food([2, 3]))
--- функции.py/Электронный попугай.py
+++ функции.py/Электронный попугай.py
@@ -1,1 +1,8 @@
+a = []
 
+
+def parrot(phrase):
+    if phrase not in a:
+        a.append(phrase)
+    else:
+        print(phrase)
